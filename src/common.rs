10824ba82b89d1a1c03b988ebfdc8188d8c20a57
zxmlysxl <zxmlysxl@gmail.com>
2025-05-12 14:23:45 +0800

    更新子模块

--- src/common.rs
+++ src/common.rs
@@ -990,7 +990,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rd.877955.xyz".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
